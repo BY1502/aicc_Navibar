0fa74d8ccacc52dbdc6c606a499b542b81b8a29e
건우 <shimgeon-u@simgeon-uui-MacBookPro.local>
2024-08-05 12:28:48 +0900

    test_1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Index';
 import Features from './components/Features/Index';
 import Navbar from './components/Navbar';
@@ -9,16 +9,19 @@ import ScrollToTop from 'react-scroll-to-top';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/aicc_Navibar/">
       <div className="App">
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/features" element={<Features />} />
-
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
+          {/* Default route to Home component */}{' '}
+          <Route path="/" element={<Navigate to="/home" />} />{' '}
+          {/* Catch all unmatched routes and redirect to Home */}{' '}
+          <Route path="*" element={<Navigate to="/home" />} />
         </Routes>
 
         <Footer />
